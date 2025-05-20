b5c8d542dd137ff71e6425a96d158ff22c184ab4
Humans99 <samuelsidabalok12@gmail.com>
2025-05-20 22:26:07 +0700

    feat: Implement DashboardLayout with Menu and Navbar components

---
+++ src/app/(dashboard)/layout.tsx
@@ -0,0 +1,30 @@
+import Image from "next/image";
+import Link from "next/link";
+import Menu from "@/components/Menu";
+import Navbar from "@/components/Navbar";
+
+export default function DashboardLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <body className="flex h-screen">
+      {/* Left */}
+      <div className="w-[14%] md:w-[8%] lg:w-[16%] xl:w-[14%] bg-white p-4 border-r border-gray-300">
+        <Link
+          href="/"
+          className="flex items-center justify-center gap-2 lg:justify-start"
+        >
+          <Image src="/logo.png" alt="Logo" width={32} height={32} />
+          <span className="hidden lg:block font-bold text-lg">School</span>
+        </Link>
+        <Menu />
+      </div>
+      {/* Right */}
+      <div className="w-[86%] md:w-[92%] lg:w-[84%] xl:w-[86%] bg-[#f7f8fa] overflow-scroll">
+        <Navbar />
+      </div>
+    </body>
+  );
+}
